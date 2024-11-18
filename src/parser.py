10ed490ef0698d1f0b587ba6a7a9775bcbca0890
RealAzzmi <azmibazeid@gmail.com>
2024-11-18 10:11:14 +0700

    feat: implement parser class prototype

---
+++ src/parser.py
@@ -0,0 +1,41 @@
+import cost
+
+from typing import List
+from lexer import Token
+
+class Node:
+    def __init__(self):
+        pass
+        
+        
+    token_type: Token
+    value: str | None # value bernilai None kalau tidak ada maknanya, contohnya untuk token_type SELECT, UPDATE, dll.
+               # value bernilai sesuatu jika ada maknanya, contohnya untuk token_type TABLE, ATTRIBUTE.
+    
+    
+class QueryPlan:
+    # Ini terserah mau dibuat kayak gimana.
+    # Yang jelas minimal harus ada:
+    #   1. select: mengconsider index-index yang ada, operasi-operasi yang dilakukan
+    #   2. sorting: external sort-merge
+    #   3. join: (indexed) nested-loop, block nested-loop, merge, hybrid merge, hash.
+    # Apakah ada yang kurang?
+    pass
+
+class ParsedQueryTree:
+    root: Node
+    childs: List[Node] = []
+    
+    query_plan: QueryPlan | None
+    
+
+    
+    
+class ParsedQuery:
+    parsed_query_tree: ParsedQueryTree
+    query: str # ini hasil query yang sudah dimodify sehingga sesuai dengan query tree yang sudah teroptimisasi
+    
+    def get_cost(self) -> float:
+        return cost.calculate_cost(self.query_tree)
+    
+    
