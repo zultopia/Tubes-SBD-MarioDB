129779bc37c37275fa998d204de76c5d2baa951c
Venantius Sean Ardi Nugroho <seanardi4@gmail.com>
2024-11-20 13:22:00 +0700

    lexer not done

--- src/lexer.py
+++ src/lexer.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import re
 from typing import Any, List, Tuple
 
 class Token(Enum):
@@ -11,6 +12,8 @@ class Token(Enum):
     WHERE = 'where'
     ORDER_BY = 'order by'
     LIMIT = 'limit'
+    ON = 'on'
+    NATURAL = 'natural'
 
     # Logical operators
     AND = 'and'
@@ -23,27 +26,117 @@ class Token(Enum):
     LESS = '<'
     LESS_EQ = '<='
     EQ = '='
+    NEQ = '<>'
 
     # Literals
-    NUMBER = None # Contoh: 3.14 atau 19
-    STRING = None # 'John' (btw ini termasuk single quotation marksnya ya)
+    NUMBER = 1 # Contoh: 3.14 atau 19
+    STRING = 2 # 'John' (btw ini termasuk single quotation marksnya ya)
 
     # Others
     ASTERISK = '*'
     COMMA = ','
-    TABLE = None # buat nama tabel atau hasil aliasing table contoh 'SELECT ATTRIBUTE from TABLE1, TABLE2, TABLE3;' 
-    ATTRIBUTE = None # buat nama attribute contoh 'select ATTRIBUTE from TABLE;' atau 'select TABLE.ATTRIBUTE from TABLE;'
+    TABLE = 3 # buat nama tabel atau hasil aliasing table contoh 'SELECT ATTRIBUTE from TABLE1, TABLE2, TABLE3;' 
+    ATTRIBUTE = 4 # buat nama attribute contoh 'select ATTRIBUTE from TABLE;' atau 'select TABLE.ATTRIBUTE from TABLE;'
     DOT = '.'
     OPEN_PARANTHESIS = '('
     CLOSE_PARANTHESIS = ')'
+    ASC = 'ascending'
+    DES = 'descending'
+    WS = None # tokens white spaces
 
     SEMICOLON = ';'
 
-
 class Lexer:
+    # assumption:
+    # query is in a single line
     query_string: str
     def __init__(self, query_string):
+        #explanation on state:
+        # 0:expecting attribute, used for after SELECT,DOT
+        # 1:expecting table, used for after FROM   
+        self.state = 0
         self.query_string = query_string
-    
+        self.regex_patterns = [
+    #some notes:
+    #(?i) means case insensitive
+
+    # Basic SQL keywords
+    (Token.SELECT, r"(?i)\bselect\b"),   
+    (Token.UPDATE, r"(?i)\bupdate\b"),
+    (Token.FROM, r"(?i)\bfrom\b"),
+    (Token.AS, r"(?i)\bas\b"),
+    (Token.JOIN, r"(?i)\bjoin\b"),
+    (Token.WHERE, r"(?i)\bwhere\b"),
+    (Token.ORDER_BY, r"(?i)\border\s+by\b"),
+    (Token.LIMIT, r"(?i)\blimit\b"),
+    (Token.ON, r"(?i)\bon\b"),
+    (Token.NATURAL, r"(?i)\bnatural\b"),
+
+    # Logical operators
+    (Token.AND, r"(?i)\band\b"),
+    (Token.NOT, r"(?i)\bnot\b"),
+    (Token.OR, r"(?i)\bor\b"),
+
+    # Comparison operators
+    (Token.GREATER, r">"),      
+    (Token.GREATER_EQ, r">="),   
+    (Token.LESS, r"<"),         
+    (Token.LESS_EQ, r"<="),      
+    (Token.EQ, r"="),           
+    (Token.NEQ, r"<>"),          
+
+    # Literals
+    (Token.NUMBER, r"\b\d+(\.\d+)?\b"),  
+    (Token.STRING, r"""(['"])(?:(?=(\\?))\2.)*?\1"""),     
+
+    # Others
+    (Token.ASTERISK, r"\*"),
+    (Token.COMMA, r","),    
+    (Token.TABLE, r"[a-zA-Z_][a-zA-Z0-9_]*"),
+    (Token.ATTRIBUTE, r"[a-zA-Z_][a-zA-Z0-9_]*"),   
+    (Token.DOT, r"\."),                  
+    (Token.OPEN_PARANTHESIS, r"\("),     
+    (Token.CLOSE_PARANTHESIS, r"\)"),    
+    (Token.ASC, r"(?i)\bascending\b"),   
+    (Token.DES, r"(?i)\bdescending\b"),           
+
+    (Token.SEMICOLON, r";")              
+]
     def tokenize(self) -> List[Tuple[Token, Any]]:
-        return []
+        result = []
+        tempQuery = self.query_string
+        while len(tempQuery) > 0:
+            isMatch = None
+            for tokenExpr in self.regex_patterns:
+                token, regex = tokenExpr
+                isMatch = re.match(regex,tempQuery)
+                if isMatch:
+                    tokenValue = token.value
+                    actualValue = isMatch.group()
+                    avLength = len(actualValue)
+                    if type(tokenValue) == int:
+                        if token == Token.NUMBER:
+                            result.append((token, float(actualValue)))
+                        elif token in {Token.ATTRIBUTE, Token.TABLE}:
+                            if self.state == 0:
+                                result.append((Token.ATTRIBUTE, actualValue))
+                            else:
+                                result.append((Token.TABLE, actualValue))
+                        else:
+                            result.append((token, actualValue))
+                    else:
+                        if token == Token.SELECT or token == Token.DOT:
+                            self.state = 0
+                        elif token == Token.FROM:
+                            self.state = 1
+                        result.append((token,actualValue))
+                    tempQuery = tempQuery[avLength:].lstrip()
+                    break # so that if match is found, y'don't have to loop through the entire thing
+            if isMatch is None: #if no patterns match return None
+                return
+
+
+        self.state=0
+        return result
+    
+print(Lexer("SELECT age1, prodi FROM user JOIN prodis ON user.prodiid = prodis.prodiid WHERE age1 > 20;").tokenize())
--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -3,6 +3,31 @@ from src.lexer import Lexer, Token
 
 def test_tokenize():
     assert Lexer("select name from person;").tokenize() == [
-        (Token.SELECT, 'select'), (Token.ATTRIBUTE, 'name'), (Token.FROM, 'from'),
-        (Token.TABLE, 'person')
+        (Token.SELECT, 'select'), (Token.ATTRIBUTE, 'name'),(Token.FROM, 'from'),
+        (Token.TABLE, 'person'),(Token.SEMICOLON, ';')
+    ]
+
+    assert Lexer("SELECT age1, prodi FROM user JOIN prodis ON user.prodiid = prodis.prodiid WHERE age1 > 20;").tokenize() ==[
+    (Token.SELECT, 'SELECT'),
+    (Token.ATTRIBUTE, 'age1'),
+    (Token.COMMA, ','),
+    (Token.ATTRIBUTE, 'prodi'),
+    (Token.FROM, 'FROM'),
+    (Token.TABLE, 'user'),
+    (Token.JOIN, 'JOIN'),
+    (Token.TABLE, 'prodis'),
+    (Token.ON, 'ON'),
+    (Token.ATTRIBUTE, 'user'),
+    (Token.DOT, '.'),
+    (Token.ATTRIBUTE, 'prodiid'),
+    (Token.EQ, '='),
+    (Token.ATTRIBUTE, 'prodis'),
+    (Token.DOT, '.'),
+    (Token.ATTRIBUTE, 'prodiid'),
+    
+    (Token.WHERE, 'WHERE'),
+    (Token.ATTRIBUTE, 'age1'),
+    (Token.GREATER, '>'),
+    (Token.NUMBER, 20),
+    (Token.SEMICOLON, ';')
     ]
