d6ca3f6886d66f51e7dfeea073de2d4182811ba9
ChristopherBrian <cbnnolan@gmail.com>
2024-11-28 00:56:45 +0700

    refactor: execute query fucntion of Query Processor component

--- Query Processor/classes.py
+++ Query Processor/classes.py
@@ -1,124 +1,41 @@
 # Query Processor sangatlah mantap
 # Import komponen yang lain dulu
-import sys
-import os
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.extend([
-    project_root,
-    os.path.join(project_root, 'query-optimizer')
-])
+from typing import List, Union, Dict, Optional
 
 # Storage Manager
 from StorageManager.classes import StorageManager
 # Query Optimizer
-from parser import parse
+from QueryOptimizer.parse_tree import ParseTree
+from QueryOptimizer.query_optimizer import get_parse_tree
+from QueryOptimizer.from_parse_tree import from_parse_tree
 
 # Kita buat class interface untuk objek-objek yang digunakan
 class Rows:
-    def __init__(self, data, rows_count):
+    def __init__(self, data: Dict[str, List[Dict[str, Union[int, str]]]], rows_count: int):
         self.data = data
         self.rows_count = rows_count
 
 class ExecutionResult:
-    def __init__(self, transaction_id, timestamp, message, data, query, manager):
+    def __init__(self, transaction_id: int, timestamp: str, message: str, data_before: Optional[Union[Rows, int]], data_after: Optional[Union[Rows, int]], status: str, query: str):
         self.transaction_id = transaction_id
         self.timestamp = timestamp
         self.message = message
-        self.data = data
+        self.data_before = data_before
+        self.data_after = data_after
+        self.status = status
         self.query = query
 
 # Sekarang query processornya
 class QueryProcessor:
-    def __init__(self, query):
+    def __init__(self):
         # Instansiasi semua komponen
         # Instansiasi Query Processor
-        # Instansiasi Storage Manager
         # Instansiasi Concurrency Control Manager
-        # Instansiasi query
-        self.transaction_id = -1 # placeholder value, belum tau transaction id untuk semua query atau hanya query transaction
-        self.query = query
+        # Intansiasi Storage Manager
         self.storage_manager = StorageManager()
-        # Placeholder, belum pakai data dari file sesungguhnya
-        self.storage_manager.data = {
-            "Student": [
-                {"StudentID": 1, "FullName": "Alice", "GPA": 3.5},
-                {"StudentID": 2, "FullName": "Bob", "GPA": 3.8},
-            ]
-        }
-    
-    # Setter getter
-    # Awalnya hanya dibuat untuk kepentingan testing
-    def set_query(self, query):
-        self.query = query
-        
-    def get_query(self):
-        return self.query
-    
-    # Intinya deh
     def execute_query(self):
-        # parse query
-        parse_tree = self.parse_query()
-        # cari node select
-        select_list_node = parse_tree.find("SelectList")
-        columns = self._extract_columns(select_list_node)
-        # cari node from
-        from_list_node = parse_tree.find("FromList")
-        table, alias = self._extract_table_and_alias(from_list_node)
-        # bentuk obyek DataRetrieval untuk storage manager
-        data_retrieval = DataRetrieval(
-            table=[table],  # List format expected
-            columns=columns,
-            conditions=[]
-        )
-        # ambil data menggunakan storage manager
-        rows = self.storage_manager.read_block(data_retrieval)
-        # handling jika query menggunakan alias untuk nama tabel
-        if alias:
-            rows.data = [
-                {f"{alias}.{col}": value for col, value in row.items()}
-                for row in rows.data
-            ]
-        # bentuk obyek executionresult sebagai hasil eksekusi query
-        result = ExecutionResult(
-            transaction_id=self.transaction_id,
-            timestamp="2024-11-22 12:00:00",  # placeholder value, nanti diganti
-            message="Query executed successfully", # placeholder, belum handle error messgae
-            data=rows,
-            query=self.query
-        )
-        print(result)
-        return result
-
-    # parse query menggunakan query optimizer
-    def parse_query(self):
-        return parse(self.query)
-
-    # fungsi-fungsi pembantu untuk mengolah node sebelum pemrosesan
-    def _extract_columns(self, select_list_node):
-        columns = []
-        for field_node in select_list_node.children:
-            if field_node.root == "Field":
-                column = self._extract_column_name(field_node)
-                columns.append(column)
-        return columns
-
-    def _extract_column_name(self, field_node):
-        children = field_node.children
-        if len(children) == 1:
-            return children[0].value
-        elif len(children) == 3:
-            return f"{children[0].value}.{children[2].value}"
-
-    def _extract_table_and_alias(self, from_list_node):
-        table_ref_node = from_list_node.find("TableReference")
-        table = table_ref_node.children[0].value
-        alias = None
-        if len(table_ref_node.children) > 1:
-            alias = table_ref_node.children[2].value
-        return table, alias
-        
-    # Fungsi yang berhubungan dengan storage manager
-    # Fungsi yang melakukan parsing dengan memanggil Query Optimizer
-    def parse_query(self):
-        parse_tree = parse(self.query)
-        return parse_tree
+        query_string = input('Please enter your query: ')
+        tree = get_parse_tree(query_string)
+        query_plan = from_parse_tree(tree)
+        query_plan.print()
+        print(tree)
