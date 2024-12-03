94e6169c0de09e3f243b1efcdba759b745ccf263
Mesach Harmasendro <13522117@std.stei.itb.ac.id>
2024-12-03 23:31:04 +0700

    feat: delete redundant file

--- tes.py
+++
@@ -1,8 +0,0 @@
-from FailureRecoveryManager.FailureRecoveryManager import FailureRecoveryManager
-from FailureRecoveryManager.RecoverCriteria import RecoverCriteria
-
-if __name__ == "__main__":
-    com = FailureRecoveryManager()
-    com.recover(RecoverCriteria(None, 101))
-    
-    print("last_log \n", com._wh_logs)
