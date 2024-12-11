d73dbd7ab9eb5a400a5cf9145232d0caa198df44
ChristopherBrian <cbnnolan@gmail.com>
2024-12-11 13:49:17 +0700

    refactor: end client process command in main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # semua komponen jadiin satu di sini
 from QueryProcessor.classes import QueryProcessor
-from QueryOptimizer.parse_tree import ParseTree
 
 QueryProcessor = QueryProcessor()
-while(True) :
-    query_string = input('Please enter your query: ')
-    if (query_string == "\\q") :
-        break
-    QueryProcessor.execute_query(query_string)
+
+while True:
+    try:
+        query_string = input('Please enter your query: ').strip()
+        if query_string == "\\q":
+            print("Exiting...")
+            break
+        elif not query_string:
+            print("Empty query, please enter a valid SQL command or '\\q' to quit.")
+            continue
+        else:
+            QueryProcessor.execute_query(query_string)
+    except Exception as e:
+        print(f"Error: {e}")
