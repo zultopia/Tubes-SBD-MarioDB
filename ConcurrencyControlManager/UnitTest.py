01392041759cd360cbb0a666add7b82f9f538e54
Steven <98434141+Zechtro@users.noreply.github.com>
2024-12-13 19:00:27 +0700

    [chore] comment unused variables

--- ConcurrencyControlManager/UnitTest.py
+++ ConcurrencyControlManager/UnitTest.py
@@ -173,16 +173,16 @@ class TestConcurrencyControlManager(unittest.TestCase):
         
         row1_table1 = Row(table1, PrimaryKey(1), {'col1': 1, 'col2': 'A'})
         row2_table1 = Row(table1, PrimaryKey(2), {'col1': 2, 'col2': 'A'})
-        row3_table1 = Row(table1, PrimaryKey(3), {'col1': 1, 'col2': 'B'})
+        # row3_table1 = Row(table1, PrimaryKey(3), {'col1': 1, 'col2': 'B'})
         
-        row1_table2 = Row(table2, PrimaryKey(1), {'col1': 1, 'col2': 'A'})
+        # row1_table2 = Row(table2, PrimaryKey(1), {'col1': 1, 'col2': 'A'})
         row2_table2 = Row(table2, PrimaryKey(2), {'col1': 1, 'col2': 'B'})
-        row3_table2 = Row(table2, PrimaryKey(3), {'col1': 1, 'col2': 'A'})
+        # row3_table2 = Row(table2, PrimaryKey(3), {'col1': 1, 'col2': 'A'})
         
-        cell1_row1_table1 = Cell(table1, row1_table1, row1_table1.pkey, 'col1', 1)
-        cell2_row1_table1 = Cell(table1, row1_table1, row1_table1.pkey, 'col2', 'A')
-        cell1_row2_table1 = Cell(table1, row2_table1, row2_table1.pkey, 'col1', 2)
-        cell2_row2_table1 = Cell(table1, row2_table1, row2_table1.pkey, 'col2', 'A')
+        # cell1_row1_table1 = Cell(table1, row1_table1, row1_table1.pkey, 'col1', 1)
+        # cell2_row1_table1 = Cell(table1, row1_table1, row1_table1.pkey, 'col2', 'A')
+        # cell1_row2_table1 = Cell(table1, row2_table1, row2_table1.pkey, 'col1', 2)
+        # cell2_row2_table1 = Cell(table1, row2_table1, row2_table1.pkey, 'col2', 'A')
         
         ccm = ConcurrencyControlManager(algorithm='tes')
         
@@ -229,15 +229,23 @@ class TestConcurrencyControlManager(unittest.TestCase):
         self.assertEqual(len(ccm.waiting_list), 8)
         
         res10 = ccm.validate_object(TransactionAction(tid1, "commit", None, None, None))
+        self.assertTrue(res10.allowed)
+        self.assertEqual(res10.status, "commit")
         self.assertEqual(len(ccm.waiting_list), 6)
         
         res11 = ccm.validate_object(TransactionAction(tid4, "commit", None, None, None))
+        self.assertFalse(res11.allowed)
+        self.assertEqual(res11.status, "wait")
         self.assertEqual(len(ccm.waiting_list), 7)
         
-        res11 = ccm.validate_object(TransactionAction(tid3, "commit", None, None, None))
+        res12 = ccm.validate_object(TransactionAction(tid3, "commit", None, None, None))
+        self.assertFalse(res12.allowed)
+        self.assertEqual(res12.status, "wait")
         self.assertEqual(len(ccm.waiting_list), 8)
         
-        res11 = ccm.validate_object(TransactionAction(tid2, "commit", None, None, None))
+        res13 = ccm.validate_object(TransactionAction(tid2, "commit", None, None, None))
+        self.assertTrue(res13.allowed)
+        self.assertEqual(res13.status, "commit")
         self.assertEqual(len(ccm.waiting_list), 0)
         
         # for t_action in ccm.waiting_list:
@@ -245,15 +253,15 @@ class TestConcurrencyControlManager(unittest.TestCase):
         
     def test_deadlock(self):
         table1 = Table('table1')
-        table2 = Table('table2')
+        # table2 = Table('table2')
         
         row1_table1 = Row(table1, PrimaryKey(1), {'col1': 1, 'col2': 'A'})
         row2_table1 = Row(table1, PrimaryKey(2), {'col1': 2, 'col2': 'A'})
-        row3_table1 = Row(table1, PrimaryKey(3), {'col1': 1, 'col2': 'B'})
+        # row3_table1 = Row(table1, PrimaryKey(3), {'col1': 1, 'col2': 'B'})
         
-        row1_table2 = Row(table2, PrimaryKey(1), {'col1': 1, 'col2': 'A'})
-        row2_table2 = Row(table2, PrimaryKey(2), {'col1': 1, 'col2': 'B'})
-        row3_table2 = Row(table2, PrimaryKey(3), {'col1': 1, 'col2': 'A'})
+        # row1_table2 = Row(table2, PrimaryKey(1), {'col1': 1, 'col2': 'A'})
+        # row2_table2 = Row(table2, PrimaryKey(2), {'col1': 1, 'col2': 'B'})
+        # row3_table2 = Row(table2, PrimaryKey(3), {'col1': 1, 'col2': 'A'})
         
         cell1_row1_table1 = Cell(table1, row1_table1, row1_table1.pkey, 'col1', 1)
         cell2_row1_table1 = Cell(table1, row1_table1, row1_table1.pkey, 'col2', 'A')
